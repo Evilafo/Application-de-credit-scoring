c02a13d4ef5480d0ebd2b692f674ebeb0887f66a
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-02-17 14:37:18 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -109,7 +109,8 @@ def main() :
     html_temp = """
 
     <div style="background-color: transparent; padding:5px; border-radius:5px; border-bottom:solid; border-bottom-color:blue; ">
-    <h1 style="text-align:center">Tableau de bord Scoring Credit 📈📊💰</h1>
+    <h1 style="text-align:center">Tableau de bord Scoring Credit </h1>
+    📈📊💰
     </div>
     <p style="font-size: 20px; font-weight: bold; text-align:center">Aide à la décision de crédit…</p>
     """
