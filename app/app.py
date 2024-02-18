8e13f541f31983a467ef8b89494144b2fe46915b
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-02-18 18:30:16 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -238,7 +238,9 @@ def main() :
     #Customer solvability display
     st.header("**Analyse du dossier client**")
     prediction = load_prediction(sample, chk_id, clf)
-    st.write("**Probabilité de défaut : **{:.0f} %".format(round(float(prediction)*100, 2)))
+    #st.write("**Probabilité de défaut : **{:.0f} %".format(round(float(prediction)*100, 2)))
+    st.markdown("** rainbow[Probabilité de défaut] :)
+    #rainbow[colors]
 
     #Compute decision according to the best threshold
     #if prediction <= xx :
