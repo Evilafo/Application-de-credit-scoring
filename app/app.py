4bc529bb38c7e2fa3275d34c74ff16142445a5f4
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-02-18 20:53:43 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -241,11 +241,12 @@ def main() :
     prediction = load_prediction(sample, chk_id, clf)
     #Calcul probabilite
     ppp = (round(float(prediction)*100,2))
+    nb = round(float(prediction)*100)
     #ppp2 = round(prediction*100)
     
     #st.write("**Probabilité de défaut : **{:.0f} %".format(round(float(prediction)*100, 2)))
     st.markdown(''':rainbow['Probabilité de défaut']''')
-    #st.markdown(f""" Probabilité de défaut : {round(float(prediction)*100)} """)
+    st.markdown(f""" Probabilité de défaut : {nb} """)
     #st.markdown(f"""Probabilité de défaut : {round(float(prediction)*100)} """)
     #rainbow[colors]
 
