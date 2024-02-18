5d8eed20ce903d4b462583dba7e1d5bd327ed612
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-02-18 19:49:37 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -240,11 +240,11 @@ def main() :
     st.header("**Analyse du dossier client**")
     prediction = load_prediction(sample, chk_id, clf)
     #Calcul probabilite
-    ppp = (round(float(prediction)*100,0))
+    ppp = (round(float(prediction)*100,2))
     
     #st.write("**Probabilité de défaut : **{:.0f} %".format(round(float(prediction)*100, 2)))
     st.markdown(''':rainbow['Probabilité de défaut']''')
-    st.markdown(f"""Probabilité de défaut : {round(float(prediction)*100, 2)} """)
+    st.markdown(f"""Probabilité de défaut : {round(float(prediction)*100, 0)} """)
     #rainbow[colors]
 
     #Compute decision according to the best threshold
