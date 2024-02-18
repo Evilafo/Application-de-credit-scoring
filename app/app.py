b9e35067e3edb1f78913c903b3e9263e22dcce51
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-02-18 19:11:10 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -244,7 +244,7 @@ def main() :
     
     #st.write("**Probabilité de défaut : **{:.0f} %".format(round(float(prediction)*100, 2)))
     st.markdown(''':rainbow['Probabilité de défaut']''')
-    st.markdown(f""" {:.0f} %.{ppp} """)
+    st.markdown(f""" {:.0ppp} """)
     #rainbow[colors]
 
     #Compute decision according to the best threshold
