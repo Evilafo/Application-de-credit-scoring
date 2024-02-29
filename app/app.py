b8d697f61b8eae75c300f0eeb07037d1b0d8ea6e
evilafo <evi2846@gmail.com>
2024-02-29 18:10:39 +0000

    g

--- app/app.py
+++ app/app.py
@@ -274,15 +274,19 @@ def main() :
     elif predict < 5 :
         message = "Faible"
         couleur = "green "
+        st.markdown(f""" Probabilité de risque de défaut : <b> :green[{predict}%] </b> """, unsafe_allow_html=True)
     elif predict < 10 :
         message = "Moyen"
         couleur = "Blue "
+        st.markdown(f""" Probabilité de risque de défaut : <b> :blue[{predict}%] </b> """, unsafe_allow_html=True)
     elif predict < 20 :
         message = "Elevé"
         couleur = "orange "
+        st.markdown(f""" Probabilité de risque de défaut : <b> :orange[{predict}%] </b> """, unsafe_allow_html=True)
     elif predict >= 20 :
         message = "Très élevé"
-        couleur = "rouge"   
+        couleur = "rouge"
+        st.markdown(f""" Probabilité de risque de défaut : <b> :orange[{predict}%] </b> """, unsafe_allow_html=True)   
     
     #st.markdown(f""" Probabilité de risque de défaut : <b> :{couleur}[{predict}%] {message} green[hola] </b> """, unsafe_allow_html=True)
     #st.markdown(f""" Probabilité de risque de défaut : <b> :green[{predict}%] </b> """, unsafe_allow_html=True)
