99f60a3cded0981c7a12ae4e1d94cc9b3c0a86d6
evilafo <evi2846@gmail.com>
2024-02-29 18:21:31 +0000

    n

--- app/app.py
+++ app/app.py
@@ -270,7 +270,7 @@ def main() :
     if predict < 1 :
         message = "Très faible"
         couleur = "green "
-        st.markdown(f""" Probabilité de risque de défaut : <b> :couleur[{predict}%] {message} </b> """, unsafe_allow_html=True)
+        st.markdown(f""" Probabilité de risque de défaut : <b> :{couleur}[{predict}%] {message} </b> """, unsafe_allow_html=True)
     elif predict < 5 :
         message = "Faible"
         couleur = "green "
