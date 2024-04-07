903254d61b20c3027da68ae3873ade3c8c2f32ce
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-04-07 15:02:51 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -195,7 +195,7 @@ def main() :
         img_gallery = """<div style="display: flex;">
         <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" alt="icon" height="40" style="height: 40px;  margin-right: 10px" />
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROKs8r8Zd_xOz-qdO6Mk9bQXGh-CP4kiHqJtIsZ2CP2Q&s" alt="icon" width="40" style="width: 40px; height: 40px; margin-right: 10px; margin-bottom: 0px;" />
-        <img src="https://www.kaggle.com/static/images/site-logo.svg" alt="icon" width="40" style="width: 40px; height: 40px; margin-right: 0px; margin-bottom: 0px;" /></div>"""
+        <img src="https://www.kaggle.com/static/images/site-logo.svg" alt="icon" height="40" style="height: 40px; margin-right: 0px; margin-bottom: 0px;" /></div>"""
         st.markdown(img_gallery, unsafe_allow_html=True)
 
 
