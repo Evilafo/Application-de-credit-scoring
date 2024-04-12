13e730932eb934f60baadea3bd592357cb719fe5
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-04-12 19:52:41 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -193,7 +193,7 @@ def main() :
         <a href="https://www.linkedin.com/in/emmanuel-evilafo-838734165"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROKs8r8Zd_xOz-qdO6Mk9bQXGh-CP4kiHqJtIsZ2CP2Q&s" alt="icon" width="40" style="width: 40px; height: 40px; margin-right: 10px; margin-bottom: 0px;" /></a>
         <a href="https://www.kaggle.com/emmanuelevilafo"><img src="https://www.kaggle.com/static/images/site-logo.svg" alt="icon" height="40" style="height: 40px; margin-right: 0px; margin-bottom: 0px;" /></a></div>"""
         st.markdown(img_gallery, unsafe_allow_html=True)
-        st.markdown("&nbsp; &nbsp; &nbsp;")
+        st.markdown("&nbsp; &nbsp;")
         st.caption("© Made by Evilafo 2023. All rights reserved.")
         st.markdown(
             '<h6>By <a href="https://github.com/Evilafo">Evilafo</a></h6>',
