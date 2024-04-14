202bdacfde7975770924b160ae3abc8c3a1a4380
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-04-14 18:16:37 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -351,7 +351,19 @@ def main() :
     
 
 
-    
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+
+
+
     #else:
         #st.markdown("<i>…</i>", unsafe_allow_html=True)
         
